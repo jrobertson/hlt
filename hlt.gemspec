00540a7ece75c829151654bf9d9438b737b9cc2d
James Robertson <james@r0bertson.co.uk>
2013-10-04 11:28:19 +0100

    feature: Embedded Markdown now supported

--- hlt.gemspec
+++ hlt.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
   s.name = 'hlt'
-  s.version = '0.1.11'
+  s.version = '0.2.1'
   s.summary = 'hlt'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
   s.add_dependency('line-tree') 
+  s.add_dependency('rdiscount') 
   s.signing_key = '../privatekeys/hlt.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/hlt.rb
+++ lib/hlt.rb
@@ -3,6 +3,7 @@
 # file: hlt.rb
 
 require 'line-tree'
+require 'rdiscount'
 
 class Hlt
 
@@ -12,23 +13,24 @@ class Hlt
 
     # strip out lines which are blank or only contain a comment
     #s = raw_s.lines.to_a.reject!{|x| x[/(^\s+\#\s)|(^\s*$)/] }
-    s = raw_s
 
+    s, markdown = fetch_markdown raw_s
+    #s = raw_s
     # strip out the text from the line containing a comment
     s.gsub!(/((^#\s|\s#\s).*)/,'').strip if s[/((^#\s|\s#\s).*)/]
-    a = s.scan(/^\[([^\]]+)\]\n/).map(&:first)
+    a_code = s.scan(/^\[([^\]]+)\]\n/).map(&:first)
     s.gsub!(/\n\[[^\]]+\]\n/, " !CODE\n")
 
     s2 = s.lines.to_a.map!{|line| 
 
       hash = "(\s+\{[^\}]+\})?"
-      line.sub(/^\s+(\w+)?(?:[\.#]\w+){1,}#{hash}/) do |x| 
+      r = line.sub(/^\s+(\w+)?(?:[\.#]\w+){1,}#{hash}/) do |x| 
 
         raw_attrs = x.slice!(/\{.*\}/)
         attrs = raw_attrs[1..-2] if raw_attrs
 
         a2 = []
-        tag = x[/((?:^|\s+)\w*)#/,1] || 'div'
+        tag = x[/(^\s*\w*)[#\.]/,1] || 'div'
         tag += 'div' if tag.strip.empty?
 
         x.sub(/(?:\.\w+){1,}/) do |x2|
@@ -43,13 +45,73 @@ class Hlt
         "%s {%s}" % [tag, a2.join(', ')]
 
       end
+
+      r
     }.join
 
+
     raw_html = LineTree.new(s2).to_xml
+
     html = raw_html.gsub('!CODE').with_index do |x,i| 
-      "\n\n" + a[i].lines.map{|x| ' ' * 4 + x}.join + "\n"
+      "\n\n" + a_code[i].lines.map{|x| ' ' * 4 + x}.join + "\n"
     end
+
+    markdown.each.with_index do |x,i|
+      html.sub!(/<markdown:#{i}\/>/, RDiscount.new(x).to_html)
+    end
+
     @to_html = Rexle.new(html).xml pretty: true
+
+  end
+
+  def fetch_markdown(raw_s)
+
+    # any lines which are in the context of markdown which only contain
+    # a new line character will have spaces inserted into them
+
+    prev_line = ''
+    s = raw_s.lines.map {|line|
+      line = prev_line + "\n" if line == "\n"
+      prev_line = line[/^\s+/]
+      line
+    }.join
+
+    index, spaces, md_spaces = 0, 0, 0
+    state = :default
+    markdown = []
+
+    s2 = s.lines.map do |line|
+
+      if state == :markdown then
+        spaces = line[/^\s+/].to_s.length
+        if spaces > md_spaces then
+          markdown[index] << line[(md_spaces + 2)..-1]
+          line = ''
+        else
+          markdown[index].strip!
+          index += 1
+          state = :default
+        end
+
+      else
+
+        r = line[/^(\s+)markdown:/,1]
+
+        if r then  
+
+          state = :markdown 
+          md_spaces = r.length    
+
+          line.sub!(/markdown:/,'\0' + index.to_s)
+          markdown[index] = ''
+        end
+      end
+
+      line
+
+    end
+
+    [s2.join, markdown]
   end
 
 end
